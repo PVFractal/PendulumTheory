8ee2400b9974e0e9c2b9dac316b2fe40e3ba30e4
Peter Vahlberg <pvahlberg@zagmail.gonzaga.edu>
2022-11-18 08:10:21 -0800

    added main java classes

---
+++ src/com/company/LineMaker.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class LineMaker {
+
+    //Program Settings
+
+    ArrayList<Integer> pointList;
+    public void UpdateFile(double timeInterval) {
+
+    }
+}
